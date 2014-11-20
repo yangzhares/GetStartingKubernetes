f89685bf802f8951c98df53f5813ace17ae4eb00
clare <zhanyang@cisco.com>
2014-11-20 16:51:30 +0800

    script for initiating api server controller-manager.service unit file

---
+++ apiserver/controller-mananger.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+KUBE_LOGTOSTDERR=true
+KUBE_LOG_LEVEL=4
+KUBE_MASTER=192.168.230.3:8080
+
+cat <<EOF >/usr/lib/systemd/system/controller-manager.service
+[Unit]
+Description=Kubernetes Controller Manager
+Documentation=https://github.com/GoogleCloudPlatform/kubernetes
+
+[Service]
+ExecStart=/opt/kubernetes/bin/controller-manager \\
+    --logtostderr=${KUBE_LOGTOSTDERR} \\
+    --v=${KUBE_LOG_LEVEL} \\
+    --master=${KUBE_MASTER}
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable controller-manager
+systemctl start controller-manager
