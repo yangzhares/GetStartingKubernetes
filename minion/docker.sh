7516a5531701e4a753cf73ed79fb20ef468eb5b9
clare <zhanyang@cisco.com>
2014-11-20 16:36:06 +0800

    script for initating minion docker configuration and docker.service

---
+++ minion/docker.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+DOCKER_BRIDGE=kbr0
+DOCKER_CONFIG=/etc/sysconfig/docker
+
+cat <<EOF >$DOCKER_CONFIG
+OPTIONS=--selinux-enabled=false
+EOF
+
+source $DOCKER_CONFIG
+cat <<EOF >/usr/lib/systemd/system/docker.service
+[Unit]
+Description=Docker Application Container Engine
+Documentation=http://docs.docker.com
+After=network.target docker.socket
+Requires=docker.socket
+
+[Service]
+Type=notify
+EnvironmentFile=-$DOCKER_CONFIG
+ExecStart=/usr/bin/docker -d --bridge=$DOCKER_BRIDGE -H fd:// $OPTIONS
+LimitNOFILE=1048576
+LimitNPROC=1048576
+
+[Install]
+Also=docker.socket
+EOF
+
+systemctl daemon-reload
+systemctl enable docker
+systemctl start docker
